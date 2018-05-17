f5a04c6c7c9419dc1fc2d3b3d37d91740cb3ef53
Manpreet Kaur <mpreet2000kaur@gmail.com>
2018-05-16 21:28:35 -0500

    minified Images

--- gulpFile.js
+++ gulpFile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('css', () =>
     gulp.src('src/css/**/*.css')
@@ -10,4 +11,10 @@ gulp.task('css', () =>
         }))
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('./dist/css/'))
+);
+
+gulp.task('imageMin', () =>
+    gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
 );
